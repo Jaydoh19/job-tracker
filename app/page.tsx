ce8f954514fa51054f32e72975a7a76eb44aa6b2
JaydenBlunt <jjaydog7@gmail.com>
2026-02-27 09:58:06 -0800

    Finish navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Navbar from "@/components/navbar";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
